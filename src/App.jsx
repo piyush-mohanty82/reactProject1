7afdd590a893ea697511a991ab53aaa4a6c559d4
Piyush Mohanty <piyushmohanty509@gmail.com>
2026-03-05 13:34:11 +0530

    add product form is completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import { Link, Route,Routes, useLocation } from 'react-router-dom'
 import Home from './components/home'
 import Details from './components/Details'
+import Create from './components/Create';
 function App() {
 
   const {search,pathname} = useLocation();
   return (
       <div className='h-screen w-screen flex'>
         {(pathname != "/" || search.length > 0) && (
-          <Link to='/' className='absolute text-red-300 left-[17%] top-[3%]'>Home</Link>
+          <Link to='/' className='absolute text-black left-[17%] top-[3%]'>Home</Link>
         )}
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
             <Route path="/details/:id" element={<Details />} />
           </Routes>
       </div>
---
+++ src/components/Create.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+
+const Create = () => {
+    const [title,settitle] = useState(""); 
+    const [image,setimage] = useState(""); 
+    const [category,setcategory] = useState(""); 
+    const [price,setprice] = useState(""); 
+    const [description,setdescription] = useState("");
+    
+    const AddProductHandler = (e) => {
+        e.preventDefault();
+        const product = {
+            title,image,category,price,description
+        }
+        console.log(product);
+    }
+    return (
+        <form onSubmit={AddProductHandler} className="p-[5%] bg-sky-400 items-center w-screen h-screen flex flex-col">
+            <h1 className="mb-5 text-3xl w-1/2">Add New Product</h1>
+            <input 
+                type="url" 
+                placeholder="image link" 
+                className="text-1xl bg-zinc-100 rounded p-3 w-1/2 mb-3" 
+                onChange={(e) => setimage(e.target.value)}
+                value={image}
+            />
+            <input 
+                type="text" 
+                placeholder="title" 
+                className="text-1xl bg-zinc-100 rounded p-3 w-1/2 mb-3" 
+                onChange={(e) => settitle(e.target.value)}
+                value={title}
+            />
+            <div className="w-1/2 flex justify-between">
+                <input 
+                    type="text" 
+                    placeholder="category" 
+                    className="text-1xl bg-zinc-100 rounded p-3 w-[48%] mb-3" 
+                    onChange={(e) => setcategory(e.target.value)}
+                    value={category}
+                />
+                <input 
+                    type="number" 
+                    placeholder="price" 
+                    className="text-1xl bg-zinc-100 rounded p-3 w-[48%] mb-3" 
+                    onChange={(e) => setprice(e.target.value)}
+                    value={price}
+                />
+            </div>
+            <textarea 
+                placeholder="enter product description here"
+                className="text-1xl bg-zinc-100 rounded p-3 w-1/2 mb-3" 
+                rows="10"
+                onChange={(e) => setdescription(e.target.value)}
+                value={description}
+            ></textarea>
+            <div className="w-1/2 ">
+                <button 
+                    className='cursor-pointer py-2 px-5 border rounded border-blue-700 text-white bg-black' 
+                >
+                    Add New Product
+                </button>
+
+            </div>
+
+        </form>
+    )
+}
+export default Create;
