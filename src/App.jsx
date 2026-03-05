a220f983c3bd435cea0bd75bb6900f7ceacf4efb
Piyush Mohanty <piyushmohanty509@gmail.com>
2026-03-05 11:26:54 +0530

    organized catagory successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import { Route,Routes } from 'react-router-dom'
+import { Link, Route,Routes, useLocation } from 'react-router-dom'
 import Home from './components/home'
 import Details from './components/Details'
 function App() {
+
+  const {search,pathname} = useLocation();
   return (
       <div className='h-screen w-screen flex'>
+        {(pathname != "/" || search.length > 0) && (
+          <Link to='/' className='absolute text-red-300 left-[17%] top-[3%]'>Home</Link>
+        )}
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/details/:id" element={<Details />} />
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,16 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Nav from "./Nav";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { ProductContext } from "../utils/Context";
 import Loading from "./Loading";
+import axios from "../utils/axios";
 const Home = () => {
     const [products] = useContext(ProductContext);
-    console.log(products);
+    const {search} = useLocation();
+
+    const category = decodeURIComponent(search.split("=")[1]);//decode url to normal string
+    
+    
+    const [filteredproducts,setfilteredproducts] = useState(null);
+    const getsproductcategory  = async () => {
+        try {
+            const {data} = await axios.get(`/products/category/${category}`) 
+            setfilteredproducts(data);
+        } catch(error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        if(!filteredproducts || category == "undefined") setfilteredproducts(products);
+        if(category != "undefined") getsproductcategory();
+    },[category,products])
+    
     return products ?  (
         <>
             <Nav />
             <div className='h-full w-[85%]  p-10 pt-[5%] flex flex-wrap  overflow-y-auto'>
-                {products.map((p,i) => (
+                {filteredproducts && filteredproducts.map((p,i) => (
                     <Link key={i} to={`/details/${p.id}`} className='mr-3 mb-3 card p-3 border shadow w-[18%] h-[30vh] flex flex-col justify-center items-center'>
                     <div 
                         className='hover:scale-110 w-full h-[80%] bg-contain bg-no-repeat bg-center mb-3'
